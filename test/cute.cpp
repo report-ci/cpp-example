b5349513f6d6fec02a33ae500b1698620747982b
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2019-01-28 20:50:20 +0800

    addedCUTE

---
+++ test/cute.cpp
@@ -0,0 +1,28 @@
+#include <cute.h>
+
+#include <ide_listener.h>
+#include <xml_listener.h>
+#include <cute_runner.h>
+
+void thisIsATest() {
+    std::string first, second, expected;
+    first = "Hello";
+    second = "World";
+    expected = "Hello World";
+    ASSERT_EQUAL(expected, first + "  " + second);
+}
+
+bool runAllTests(int argc, char const *argv[]) {
+    cute::suite s { };
+    //TODO add your test here
+    s.push_back(CUTE(thisIsATest));
+    cute::xml_file_opener xmlfile(argc, argv);
+    cute::xml_listener<cute::ide_listener<>> lis(xmlfile.out);
+    auto runner = cute::makeRunner(lis, argc, argv);
+    bool success = runner(s, "AllTests");
+    return success;
+}
+
+int main(int argc, char const *argv[]) {
+    return runAllTests(argc, argv) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
