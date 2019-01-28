dcfbff06a37b3906104d62c1a2fe1522333a4c9b
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2019-01-28 19:26:46 +0800

    added cpputest

---
+++ test/cpputest.cpp
@@ -0,0 +1,28 @@
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(ClassName)
+{
+        void setup()
+        {
+        }
+        void teardown()
+        {
+        }
+};
+
+TEST(ClassName, Create)
+{
+    CHECK(true);
+    CHECK_EQUAL(1,1);
+    LONGS_EQUAL(1,1);
+    DOUBLES_EQUAL(1.000, 1.001, .01);
+    STRCMP_EQUAL("hello", "hello");
+    FAIL("The prior tests pass, but this one doesn't");
+}
+
+#include "CppUTest/CommandLineTestRunner.h"
+
+int main(int ac, char** av)
+{
+    return RUN_ALL_TESTS(ac, av);
+}
