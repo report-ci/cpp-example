644f5f0cb9e004fdcf169d49ecaaf21ad01154cf
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2019-01-29 16:12:02 +0800

    added cxxtest

---
+++ test/cxxtest.cpp
@@ -0,0 +1,22 @@
+#include <cxxtest/TestSuite.h>
+
+class MyTestSuite1 : public CxxTest::TestSuite
+{
+public:
+    void testAddition(void)
+    {
+        TS_ASSERT(1 + 1 > 1);
+        TS_ASSERT_EQUALS(1 + 1, 2);
+    }
+
+    void testXYZ()
+    {
+        TS_ABORT();
+        TS_ASSERT(0);
+    }
+
+    void testBar()
+    {
+        throw std::runtime_error("blablubb");
+    }
+};
