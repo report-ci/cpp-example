0d7bf18ceeedc2bb75a2dc34218447d492306818
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2019-01-30 13:43:36 +0800

    added x flag to redirect script

--- test/redirect.sh
+++ test/redirect.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 set -o pipefail
-
-if [[ $# -eq 3 ]]; then
-        $1 | tee $2
-elif [[ $# -eq 4 ]]; then
-    $1 $2 | tee $3
-else
-    echo "Invalid number of arguments $#"
-    exit 2
-fi
+$1 | tee $2
