f8663a5bf0e5bdd487d074a7658703adccdaded7
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2019-01-30 13:37:54 +0800

    trying to fix the script issue

--- test/redirect.sh
+++ test/redirect.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 set -o pipefail
-$1 | tee $2
+
+if [[ $# -eq 3 ]]; then
+        $1 | tee $2
+elif [[ $# -eq 4 ]]; then
+    $1 $2 | tee $3
+else
+    echo "Invalid number of arguments $#"
+    exit 2
+fi
